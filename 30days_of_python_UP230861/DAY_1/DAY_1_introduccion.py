4890647d56791a9fa1b714362acac8b8af98d4f5
UP230861 <up230861@alumnos.upa.edu.mx>
2025-06-11 23:40:44 -0600

    Update DAY_1_introduccion.py

--- 30days_of_python_UP230861/DAY_1/DAY_1_introduccion.py
+++ 30days_of_python_UP230861/DAY_1/DAY_1_introduccion.py
@@ -19,3 +19,78 @@ print(type({9.8, 3.14, 2.7}))
 print(type((9.8, 3.14, 2.7))) 
 print(type(3 == 3))  
 print(type(3 >= 3))
+
+#
+a = 10
+b = 15
+print ('numero 1 = 10 y numero 2 = 15')
+print("el resultado de la suma de los numeros es de :", a + b)
+
+print("el resultado de la resta de los numeros es de ::", a - b)
+
+print("el resultado de la multiplicacion de los numeros es de ::", a * b)
+
+print("el resultado de la modulo de los numeros es de ::", a % b)
+
+print("el resultado de la divicion de los numeros es de ::", a / b)
+
+print("el resultado de la exponente de num1 a la exponente de num2 es de:", a ** b)
+
+print("el resultado de la divicion entera de los numeros es de :", a // b)
+
+
+nombre= "AXEL"
+part_of_family='AXEL','giovanna','enrique'
+country = "mexico"
+
+
+#tipos 
+print(type(10))                  
+print(type(3.14))  
+print(type(1 + 3j)) 
+print(type('Asabeneh')) 
+print(type([1, 2, 3]))  
+print(type({'name':'Asabeneh'}))
+print(type({9.8, 3.14, 2.7}))  
+print(type((9.8, 3.14, 2.7))) 
+print(type(3 == 3))  
+print(type(3 >= 3))
+
+#parte 3
+nument = 5             
+float = 5.23            
+numero_complejo = 7 + 13         
+
+string = "clase de progra"
+
+boolean= False
+q=1
+w=2
+e=3
+list= [q,w,e]
+
+tupla = (5, 6, 7)
+
+conjunto = {1, 2, 3, 2} 
+
+diccionario = {"nombre": "alonso", "edad": 19}
+
+print("numero entero:", nument)
+print("flotante:", float)
+print("numero complejo:", numero_complejo)
+print("string:", string)
+print("boolean:", boolean)
+print("lista:", list)
+print("tupla:", tupla)
+print("conjunto:", conjunto)
+print("Diccionario:", diccionario)
+
+x1, y1 = 2, 3
+x2, y2 = 10, 8
+
+diferencia_x = x1 - y1
+diferencia_y = x2 - y2
+
+resultado = (diferencia_x**2 + diferencia_y**2) ** 0.5
+
+print(resultado)
